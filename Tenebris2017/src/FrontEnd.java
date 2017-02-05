16d7a8e4d2073bc2bcac759f7482552d72172eb2
Lucas Coimbra <lucasgabrielce@gmail.com>
2017-02-05 17:46:58 -0300

    no message

---
+++ Tenebris2017/src/FrontEnd.java
@@ -0,0 +1,13 @@
+import java.sql.SQLException;
+
+public class FrontEnd {
+
+	public static void main(String[] args) throws InstantiationException, IllegalAccessException, ClassNotFoundException, SQLException {
+		
+		Exemplo ex = new Exemplo();
+		//System.out.println("Aqui estou");
+		ex.Metodo(12, 86);
+
+	}
+
+}
---
+++ Tenebris2017/src/ObraRB.java
@@ -0,0 +1,56 @@
+public class ObraRB {
+	
+    private int id;
+    private int usuario;
+	private int relevancia;
+	private String titulo;
+	private int palavrachave;
+	private String pcString;
+ 
+	public int getId() {
+        return id;
+    }
+	
+    public void setId(int id) {
+        this.id = id;
+    }
+    
+    public int getUsuario() {
+        return usuario;
+    }
+	
+    public void setUsuario(int usuario) {
+        this.usuario = usuario;
+    }
+    
+    public int getPchave() {
+        return palavrachave;
+    }
+    public void setPchave(int pchave) {
+        this.palavrachave = pchave;
+        
+    }
+    
+    public String getTitulo() {
+        return titulo;
+    }
+    public void setTitulo(String titulo) {
+        this.titulo = titulo;
+    }
+    
+    public String getPcstring() {
+        return pcString;
+    }
+    public void setPcstring(String pcstring) {
+        this.pcString = pcstring;
+    }
+
+	public void setRelevancia(int relevancia) {
+		this.relevancia = relevancia;
+	}
+	
+	public int getRelevancia(){
+		return relevancia;
+	}
+    
+}
